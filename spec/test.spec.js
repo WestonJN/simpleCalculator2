0fc3b2ea1c595f657c3a921726f5fb6d6e53d4c0
WestonJN <westonnyirenda@gmail.com>
2019-11-22 14:33:32 +0200

    Function changes

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,34 +1,36 @@
-var calculator = require('../src/calculator');
+var calculator = require('../src/simpleCalculator');
 
-describe("Simple calculator to add two numbers", function () {
+describe("class Calculator", function () {
+   
     it("Must add two numbers", function () {
-        expect(calculator.sum(0,0)).toEqual(0);
+        expect(calculator.add(3,5)).toEqual(8);
     });
 });
 
-describe("Simple calculator to add two numbers", function () {
-    it("Must add two numbers", function () {
-        expect(calculator.sum(-1,-1)).toEqual(-2);
-    });
-}); 
-
-describe("Simple calculator to add two numbers", function () {
-    it("Must add two numbers", function () {
-        expect(calculator.sum(4,5)).toEqual(9);
-    });
-});
 describe("Simple calculator that multiplies two numbers", function () {
     it("Must multiply two numbers", function () {
-        expect(calculator.product(1,2)).toEqual(2);
+        expect(calculator.multiply(30,2)).toEqual(60);
     });
 });
 describe("Simple calculator that adds a bunch numbers", function () {
     it("Must add a bunch numbers", function () {
-        expect(calculator.sum(1,2,3,4)).toEqual(10);
+        expect(calculator.add(3,5,2)).toEqual(10);
     });
 });
-describe("Simple calculator that multiplies a bunch numbers", function () {
-    it("Must multiply a bunch numbers", function () {
-        expect(calculator.product(1,2,3,4)).toEqual(24);
+describe("class Calculator", function () {
+    it("Must add two numbers", function () {
+        expect(calculator.add(1,2)).toEqual(3);
     });
 });
+
+    it("should return last result", function () {
+        expect(calculator.last()).toEqual(3);
+    });
+    it("allow last as a parameter", function () {
+        expect(calculator.multiply("Last",5)).toEqual(15);
+    });
+    it("Should take single number as slot number", function () {
+        expect(calculator.set_slot(1));
+        expect(calculator.get_slot(1)) .toEqual(1);
+    });
+
--- src/simpleCalculator.js
+++ src/simpleCalculator.js
@@ -1,23 +1,37 @@
-module.exports = {
+class Calculator{
+    
+     add(...nums){
+        var add=0;
+        for (var i=0;i < nums.length;i++){
+            add += nums[i];
+            this.last_value =add;
+       
+        } return add;
+       
+        }
+        
+     multiply(...nums){
+         var multiply=1;
+         for (var i=0;i < nums.length;i++){
+                multiply *= nums[i];
+                thid.last_value=multiply;
+        } return multiply;
+           
+           }
 
-    sum,
-    product
-}
-
-
-function sum(...nums){
-    var sum=0;
-    for (var i=0;i < nums.length;i++){
-        sum += nums[i];
-    } return sum;
-   
+    last(){
+        return this.last_value;
+    }
+    set_slot(i){
+        this.set_slot.push(this.last_value);
+        return this.slot[i-1]
+    }  
+    get_slot(i){
+        return this.slot[i-1]
     }
     
-function product(...nums){
-     var product=1;
-     for (var i=0;i < nums.length;i++){
-            product *= nums[i];
-    } return product;
-       
-       }
-   
+}
+
+let calculator_inst = new Calculator();
+console.log(calculator_inst.add(2,5));
+console.log(calculator_inst.last("last",5));
